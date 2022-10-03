7cdffb7de5421af6ef86f1cd58eda018b34d750f
Johann Dahm <johann.dahm@gmail.com>
2022-10-03 10:26:04 -0700

    Add run script (#5)

---
+++ scripts/run_shallow_water.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+config=$1
+others=${@:2}
+
+if ! command -v yq &> /dev/null; then
+	echo "The command <yq> does not exist"
+	exit
+fi
+
+nproc=$(yq e '.grid.proc_layout[0] * .grid.proc_layout[1]' $config)
+mpirun -np $nproc shallow_water_demo $others $config
+
